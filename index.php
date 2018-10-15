438ff8716e0d6f086228b2ba16d14e53894bf7d6
mayankpatel104 <mayank.patel104@gmail.com>
2018-10-15 14:41:22 +0530

    m

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php $yourAddress = "345 S. Jefferson Ave. Suite 300 Cookeville, TN 38501 "; ?>
+<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.it/maps?q=<?php echo $yourAddress; ?>&output=embed"></iframe>
